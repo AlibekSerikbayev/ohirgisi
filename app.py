717e45b078144c82c5af86592352978da595f52d
AlibekSerikbayev <121658011+AlibekSerikbayev@users.noreply.github.com>
2024-11-18 21:42:44 +0500

    aapp

--- app.py
+++ app.py
@@ -18,30 +18,36 @@ st.markdown("> :green[Rasmni ushbu qismga yuklang]")
 file_upload = st.file_uploader("Rasm yuklash (avif, png, jpeg, gif, svg)", type=["avif", "png", "jpeg", "gif", "svg"])
 url_input = st.text_input("Yoki rasmning URL manzilini kiriting")
 
+# Modelni yuklash
+try:
+    model = load_learner('transport_model.pkl')
+except Exception as e:
+    st.error(f"Modelni yuklashda xatolik: {e}")
+    model = None
+
 # Rasm yuklash va ko'rsatish
 if file_upload or url_input:
-    if file_upload:
-        img = PILImage.create(file_upload)
-        st.image(file_upload, caption="Yuklangan rasm")
-    else:
-        try:
+    try:
+        if file_upload:
+            img = PILImage.create(file_upload)
+            st.image(file_upload, caption="Yuklangan rasm")
+        else:
             response = requests.get(url_input)
             img = PILImage.create(BytesIO(response.content))
             st.image(img, caption="URL orqali yuklangan rasm")
-        except Exception as e:
-            st.error("Rasmni yuklashda xatolik! Iltimos, URLni tekshiring.")
-
-    # Modelni yuklash
-    model = load_learner('transport_model.pkl')
-
-    # Bashorat qilish
-    pred, pred_id, probs = model.predict(img)
-    st.success(f"Bashorat: {pred}")
-    st.info(f"Ehtimollik: {probs[pred_id] * 100:.1f}%")
-
-    # Diagramma chizish
-    fig = px.bar(x=probs * 100, y=model.dls.vocab, labels={'x': "Ehtimollik (%)", 'y': "Klasslar"}, orientation='h')
-    st.plotly_chart(fig)
+        
+        if isinstance(img, PILImage) and model is not None:
+            pred, pred_id, probs = model.predict(img)
+            st.success(f"Bashorat: {pred}")
+            st.info(f"Ehtimollik: {probs[pred_id] * 100:.1f}%")
+            
+            # Diagramma chizish
+            fig = px.bar(x=probs * 100, y=model.dls.vocab, labels={'x': "Ehtimollik (%)", 'y': "Klasslar"}, orientation='h')
+            st.plotly_chart(fig)
+        else:
+            st.error("Tasvirni yoki modelni yuklashda muammo bor.")
+    except Exception as e:
+        st.error(f"Bashorat qilishda xatolik: {e}")
 
 # Sidebar qo'shimchalar
 st.sidebar.header("Qo'shimcha ma'lumotlar")
